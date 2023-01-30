86d5b86ffc58b47d84b3b0df9765267718f961ce
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-29 23:13:21 -0300

    Creating the ContactItem & FormSearchInput component so can be used inside of Contact Screen

---
+++ components/ContactItem.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { COLORS, FONTS } from "../constants/constants";
+
+export interface ContactItemProps {
+  contactName: string;
+  contactNumber: string;
+  contactCharacters: string;
+}
+
+const ContactItem: React.FC<ContactItemProps> = ({
+  contactName,
+  contactNumber,
+  contactCharacters,
+}) => {
+  return (
+    <View style={styles.contact}>
+      <View style={styles.contactContainer}>
+        <View style={styles.horizontalContainer}>
+          <View style={styles.contactSquareContainer}>
+            <Text style={styles.contactSquareContainerText}>
+              {contactCharacters}
+            </Text>
+          </View>
+          <View style={styles.contactData}>
+            <Text style={styles.contactName}>{contactName}</Text>
+            <Text style={styles.contactNumber}>{contactNumber}</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  contact: {
+    backgroundColor: COLORS.white,
+    borderRadius: 16,
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+  contactContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  horizontalContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  contactSquareContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 12,
+    width: 44,
+    height: 44,
+    backgroundColor: COLORS.lightblue,
+    marginRight: 16,
+  },
+  contactSquareContainerText: {
+    fontFamily: FONTS.primary,
+    color: COLORS.lightblueText,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  contactData: {
+    justifyContent: "space-between",
+  },
+  contactName: {
+    marginBottom: 5,
+    fontFamily: FONTS.primary,
+    color: COLORS.greySubTitle,
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  contactNumber: {
+    marginBottom: 5,
+    fontFamily: FONTS.primary,
+    color: COLORS.greyLabel,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  contactAmount: {
+    marginBottom: 5,
+    fontFamily: FONTS.primary,
+    color: COLORS.purple,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+});
+
+export default ContactItem;
---
+++ components/FormSearchInput.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+import { COLORS, FONTS } from "../constants/constants";
+
+export interface FormSearchInputProps {
+  placeholder: string;
+  value?: string;
+  handleOnChangeText: (value: string) => void;
+}
+
+const FormSearchInput: React.FC<FormSearchInputProps> = ({
+  placeholder,
+  value,
+  handleOnChangeText,
+}) => {
+  return (
+    <View style={styles.searchInput}>
+      <Image
+        style={styles.searchInputImage}
+        source={require("../assets/img/search_grey.png")}
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder={placeholder}
+        placeholderTextColor={"#9CA2B7"}
+        value={value && value}
+        onChangeText={handleOnChangeText}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchInput: {
+    backgroundColor: COLORS.white,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 12,
+    paddingVertical: 20,
+    borderRadius: 16,
+    width: '100%'
+  },
+  searchInputImage: {
+    width: 17,
+    height: 17,
+    marginRight: 20,
+  },
+  inputStyle: {
+    width: '90%',
+  },
+});
+
+export default FormSearchInput;
