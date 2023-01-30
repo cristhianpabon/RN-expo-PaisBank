e011162a445ae1fc70fd6f356c267d8eee898559
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-29 23:06:44 -0300

    Adding all the hexadecimals colors related to the design on figma to the constants file

---
+++ constants/constants.ts
@@ -0,0 +1,24 @@
+export const COLORS = {
+  primaryBlue: "#005CEE",
+  white: "#FFFFFF",
+  greyTitle: "#334154",
+  greySubTitle: "#616E7C",
+  grey: "#717E95",
+  greyLabel: "#AAAAAA",
+  greyLine: "#ACBAC3",
+  greyCheckBox: "#DBDBDB",
+  purple: "#B946FF",
+  green: "#74CC9B",
+  orange: "#EF9C55",
+  lightgrey: "#F9FAFC",
+  lightGreen: "#E4FFF0",
+  lightOrange: "#FEEAD4",
+  lightPinkTuna: "#F9B7B7",
+  lightPurple: "#EEE3FF",
+  lightblue: "#CAF0FF",
+  lightblueText: "#68C6E5",
+};
+
+export const FONTS = {
+  primary: "Arial",
+};
