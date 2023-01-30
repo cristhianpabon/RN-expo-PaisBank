b68b7792cd6c6b589757c1b8c580cc5e995d5fe3
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-29 23:14:13 -0300

    Creating ContactScreen component, also adding the ContactItem & FormSearchInput inside

---
+++ screens/ContactScreen.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import type { RootState } from "../redux/store";
+import { COLORS, FONTS } from "../constants/constants";
+
+import ContactItem from "../components/ContactItem";
+import FormSearchInput from "../components/FormSearchInput";
+
+export default function ContactScreen() {
+  const cards = useSelector((state: RootState) => state.cards.value);
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.contactWidth}>
+          <FormSearchInput
+            placeholder={"Ingresa un nombre o un número"}
+            handleOnChangeText={(text)=> console.log(text)}
+          />
+        </View>
+        <View style={styles.contacts}>
+          <Text style={[styles.contactsSectionTitle, styles.contactWidth]}>
+            Recents
+          </Text>
+          <View style={styles.contactSeparator}></View>
+          <View style={[styles.contactsList, styles.contactWidth]}>
+            <ContactItem
+              contactName={"Belen Salvador"}
+              contactNumber={"+8643307899"}
+              contactCharacters={"BS"}
+            />
+            <ContactItem
+              contactName={"Belen Salvador"}
+              contactNumber={"+8643307899"}
+              contactCharacters={"BS"}
+            />
+          </View>
+          <Text style={[styles.contactsSectionTitle, styles.contactWidth]}>
+            All
+          </Text>
+          <View style={styles.contactSeparator}></View>
+          <View style={[styles.contactsList, styles.contactWidth]}>
+            <ContactItem
+              contactName={"Belen Salvador"}
+              contactNumber={"+8643307899"}
+              contactCharacters={"BS"}
+            />
+            <ContactItem
+              contactName={"Belen Salvador"}
+              contactNumber={"+8643307899"}
+              contactCharacters={"BS"}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightgrey,
+  },
+  contactWidth: {
+    width: "90%",
+    alignSelf: "center",
+  },
+  contacts: {},
+  contactsSectionTitle: {
+    fontFamily: FONTS.primary,
+    color: COLORS.greyLabel,
+    fontSize: 16,
+    fontWeight: "400",
+    marginBottom: 16,
+  },
+  contactSeparator: {
+    borderTopWidth: 0.5,
+    borderTopColor: COLORS.greyLine,
+  },
+  contactsList: {
+    paddingTop: 24,
+    paddingBottom: 24,
+  },
+});
