c0b016e03a3f149979d0b480a7c9e9a5d88c56b9
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-30 22:48:18 -0300

    Adding the logic the load the fonts meanwhile the app is loading

--- App.tsx
+++ App.tsx
@@ -1,14 +1,40 @@
+import { useCallback, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
 import { StyleSheet } from "react-native";
 import { store } from "./redux/store";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync(); 
 
 import { BottomTabNavigator } from "./navigator/BottomTabNavigator";
 
 export default function App() {
+  const [loaded] = Font.useFonts({
+    Poppins: require("./assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (loaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer onReady={onLayoutRootView}>
         <BottomTabNavigator />
       </NavigationContainer>
     </Provider>
